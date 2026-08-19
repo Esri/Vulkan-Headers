cf100db89937f1bad29e54c628fa38549dd5efa1
julialubiszewska <julialubiszewska12@gmail.com>
2026-08-19 14:21:06 +0100

    Change version to 1.4.360

--- conanfile_rtc.py
+++ conanfile_rtc.py
@@ -5,7 +5,7 @@ from conans import ConanFile
 
 class VulkanHeadersConan(ConanFile):
     name = "Vulkan-Headers"
-    version = "1.4.353"
+    version = "1.4.360"
     url = "https://github.com/Esri/Vulkan-Headers/blob/runtimecore/"
     license = "https://github.com/Esri/Vulkan-Headers/blob/runtimecore/LICENSE.md"
     description = ("Vulkan header files and API registry")
